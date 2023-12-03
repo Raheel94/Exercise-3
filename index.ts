afc0e648dff69a1c6eeaf03dad6c9659385f5bd5
Raheel94 <muha.raheel94@gmail.com>
2023-12-03 11:19:11 +0500

    go

---
+++ index.ts
@@ -0,0 +1,16 @@
+// Exercise 3:  Write a program that checks if a given number is positive, negative, or zero.
+
+function checkNum(num:number){
+    if(num>=1){
+        console.log("Its a positive number")
+    } else if (num < 0){
+        console.log("its a negative number")
+    } else {(num = 0)
+        console.log("Zero")
+    } 
+}
+
+// Test the function
+checkNum(22);
+checkNum(-22)
+checkNum(0);
